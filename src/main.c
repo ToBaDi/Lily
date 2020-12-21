708ff85d5f33a32efb21c22188814ac3b10c7f93
Devsignerz <devsignerz@gmail.com>
2020-12-21 01:50:43 -0600

    fuuuuuck

--- src/main.c
+++ src/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include "game/game.h"
 
-int main()
-{
-    printf("Hello World!\n");
+int main(){
+	printf("%s", getFile());
+    //printf("Hello World!\n");
     return 0;
+}
+
+char* getFile(){
+	FILE *fp;
+   int c;
+
+   fp = fopen("file.txt","r");
+   while(1) {
+      c = fgetc(fp);
+      if( feof(fp) ) {
+         break;
+      }
+      return c; 
+   }
+   fclose(fp);
+   return(0);
 }
