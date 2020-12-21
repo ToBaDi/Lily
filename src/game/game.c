e9b67f30068eea1a760b04380ae5a326b557b2cb
Sarvarian <Sarvarian@Hotmail.com>
2020-12-21 07:19:54 -0200

    update game

--- src/game/game.c
+++ src/game/game.c
@@ -3,31 +3,4 @@
 
 void game_debug()
 {
-    Level level_1;
-    int x = 0, y = 0;
-
-    y = 0;
-    while (y < LEVEL_Y_SIZE)
-    {
-        x = 0;
-        while (x < LEVEL_X_SIZE)
-        {
-            level_1[x][y] = '#';
-            x++;
-        }
-        y++;
-    }
-
-    y = 0;
-    while (y < LEVEL_Y_SIZE)
-    {
-        x = 0;
-        while (x < LEVEL_X_SIZE)
-        {
-            printf("%c", level_1[x][y]);
-            x++;
-        }
-        printf("\n");
-        y++;
-    }
 }
--- src/game/game.h
+++ src/game/game.h
@@ -4,21 +4,12 @@
 #define LEVEL_X_SIZE 32
 #define LEVEL_Y_SIZE 8
 
-struct Vec2
-{
-    int x;
-    int y;
-} typedef Vec2;
-
 struct Character
 {
     char *name;
-    Vec2 pos;
     int hp;
     int damage;
 } typedef Character;
-
-typedef char Level[LEVEL_X_SIZE][LEVEL_Y_SIZE];
 
 void game_debug();
 
